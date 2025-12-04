600c0404ce253b17ebe169947d6155bfa4c98a8b
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-12-04 14:09:59 +0200

    feat(routes): register secure chat api endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\ChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/conversations', [ChatController::class, 'index']);
+    Route::get('/conversations/{conversation}', [ChatController::class, 'show']);
+    Route::post('/conversations/{conversation}/messages', [ChatController::class, 'store']);
+    Route::patch('/conversations/{conversation}/archive', [ChatController::class, 'toggleArchive']);
+    Route::patch('/conversations/{conversation}/favorite', [ChatController::class, 'toggleFavorite']);
+});
