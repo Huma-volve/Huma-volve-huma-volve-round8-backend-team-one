bfda1e9fdade1e349885fc9ec9d1f822c2a7f96e
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-12-04 11:46:36 +0200

    feat(security): define private chat channel authorization

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\User;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('chat.{conversationId}', function (User $user, int $conversationId) {
+    return $user->conversations()->where('id', $conversationId)->exists();
 });
