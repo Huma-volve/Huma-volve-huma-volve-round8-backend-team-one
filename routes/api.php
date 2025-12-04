d8798a4b69fcfe6af52b318e2dbfa84a83ba77df
nada <nsheta038@gmail.com>
2025-12-04 18:11:53 +0200

    reviews and notifications

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\NotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('store/review', [ReviewController::class, 'store']);
+Route::get('doctor/reviews', [ReviewController::class, 'reviews']);
+Route::post('/doctor/review/{review}/reply', [ReviewController::class, 'reply']);
+Route::get('/notifications', [NotificationController::class, 'index']);
+Route::get('notifications/unread', [NotificationController::class, 'unread']);
+Route::post('notifications/{id}/read', [NotificationController::class, 'markAsRead']);
+
