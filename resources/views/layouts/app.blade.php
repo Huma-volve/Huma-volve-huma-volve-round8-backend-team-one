c84dfa977ede34e006371e9abe95b0729644d8f2
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-12-10 19:06:20 +0200

    feat(doctor): extracted the Footer into resources/views/layouts/partials/footer.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -264,10 +264,7 @@
             @endif
         </main>
 
-        <!-- Footer -->
-        <footer class="p-4 text-center text-xs text-slate-400">
-            &copy; {{ date('Y') }} HumaVolve Medical System. All rights reserved.
-        </footer>
+        @include('layouts.partials.footer')
     </div>
 </body>
 </html>
