51383e01cfcabfefd7465a5fbe412b02e9325e7b
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-12-14 00:45:38 +0200

    feat(doctor): fixed doctor java script bugs for the chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
 import faqManager from './admin/faq-manager';
+import { initDoctorChat } from './doctor-chat';
+
+window.initDoctorChat = initDoctorChat;
 
 Alpine.data('faqManager', faqManager);
 
