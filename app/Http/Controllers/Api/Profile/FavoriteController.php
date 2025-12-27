f0a95f8ba3896c9f3b84a5908a1dea22bb57670e
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-12-27 12:18:19 +0200

    wi

--- app/Http/Controllers/Api/Profile/FavoriteController.php
+++ app/Http/Controllers/Api/Profile/FavoriteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Profile;
 use App\Models\Favorite;
 use App\Traits\ApiResponse;
 use App\Models\FavoriteDoctor;
+use App\Models\PatientProfile;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,13 @@ class FavoriteController extends Controller
     use ApiResponse;
     public function index(){
 
-        $favorits =    Favorite::where('patient_id', Auth::id())
+        $patient = PatientProfile::where('user_id', Auth::id())->first();
+        if(!$patient){
+            return $this->errorResponse('Patient not found!',404);
+        }
+        $favorits =    Favorite::where('patient_id', $patient->id)
+            ->with('doctor')
+            
                                     ->get();
 
         if($favorits->count() > 0){
